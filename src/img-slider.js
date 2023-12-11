f4d2362b88872918b2e219ba35dd6442187a7787
David Olona <damilear@outlook.com>
2023-12-11 18:53:00 -0500

    Add slideshow functions

--- src/img-slider.js
+++ src/img-slider.js
@@ -1,9 +1,46 @@
+/* eslint-disable no-use-before-define */
+/* eslint no-plusplus: ["error", { "allowForLoopAfterthoughts": true }] */
 
 
+// Global
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
 // Add slider to node: Input:'class', Return -> None
 export default function imgSlider(cls) {}
 
+// Function to show slides
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 
 function createImgSlider() {
     
